44735bc2ece8779a5a144ee7f5d26500ad7b1dae
Sol Fanton <sfanton.develop@gmail.com>
2024-11-18 16:03:05 -0300

    Se ajustan formatos para que corran tests correctamente de segunda prueba tecnica certificacion analisis de datos freecodecamp

--- freecodecamp/Python-Data-Analysis-Certification/boilerplate-demographic-data-analyzer-main/demographic_data_analyzer.py
+++ freecodecamp/Python-Data-Analysis-Certification/boilerplate-demographic-data-analyzer-main/demographic_data_analyzer.py
@@ -6,31 +6,32 @@ def calculate_demographic_data(print_data=True):
     df = pd.read_csv('./adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = df.value_counts(df["race"])
+    race_count = df["race"].value_counts().values
 
     # What is the average age of men?
-    average_age_men = df[df["sex"] == "Male"]["age"].mean()
+    average_age_men = df[df["sex"] == "Male"]["age"].mean().round(1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = (df[df["education"] == "Bachelors"].shape[0] / df.shape[0]) * 100
+    percentage_bachelors = round((df[df["education"] == "Bachelors"].shape[0] / df.shape[0]) * 100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = df["education"].isin(['Bachelors', 'Masters', 'Doctorate'])
-    lower_education = ~df["education"].isin(['Bachelors', 'Masters', 'Doctorate'])
+    higher_education = df[df["education"].isin(['Bachelors', 'Masters', 'Doctorate'])]
+    lower_education = df[~df["education"].isin(['Bachelors', 'Masters', 'Doctorate'])]
 
     # percentage with salary >50K
-    higher_education_rich = ( df[(higher_education) & (df["salary"] == ">50K")].shape[0] / df.shape[0] ) * 100
-    lower_education_rich = ( df[(lower_education) & (df["salary"] == ">50K")].shape[0] / df.shape[0] ) * 100
+    higher_education_rich = round((higher_education[higher_education["salary"] == ">50K"].shape[0] / higher_education.shape[0]) * 100, 1)
+    lower_education_rich = round((lower_education[lower_education["salary"] == ">50K"].shape[0] / lower_education.shape[0]) * 100, 1)
+
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
     min_work_hours = df["hours-per-week"].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
     num_min_workers = df[df["hours-per-week"] == min_work_hours]
-    rich_percentage = ( num_min_workers[df["salary"] == ">50K"].shape[0] / df.shape[0] ) * 100
+    rich_percentage = int((num_min_workers[num_min_workers["salary"] == ">50K"].shape[0] / num_min_workers.shape[0]) * 100)
 
     # What country has the highest percentage of people that earn >50K?
     gc = df.groupby(['native-country', 'salary']).size().unstack(fill_value=0)
@@ -38,14 +39,15 @@ def calculate_demographic_data(print_data=True):
     gc['percentage_earning_above_50K'] = (gc['>50K'] / gc['total']) * 100
 
     highest_earning_country = gc[gc['percentage_earning_above_50K'] == gc['percentage_earning_above_50K'].max()].index.tolist()[0]
-    highest_earning_country_percentage = gc['percentage_earning_above_50K'].max()
+    highest_earning_country_percentage = gc['percentage_earning_above_50K'].max().round(1)
 
     # Identify the most popular occupation for those who earn >50K in India.
     filtered = df[(df["native-country"] == "India") & (df["salary"] == ">50K")]
     occupations_IN = filtered.groupby(["occupation"]).size().reset_index(name='count')
     max_IN_occupation = occupations_IN["count"].max()
 
-    top_IN_occupation = occupations_IN[occupations_IN["count"] == max_IN_occupation]["occupation"]
+    top_IN_occupation = occupations_IN[occupations_IN["count"] == max_IN_occupation].iloc[0]["occupation"]
+
 
     # DO NOT MODIFY BELOW THIS LINE
 
